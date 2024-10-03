7abe80417d8a5550337b4594dc70231aa4019bda
PeejayRocas <rocaspeejay8@gmail.com>
2024-10-03 10:02:39 +0800

    03VariableDeclaration.cpp

---
+++ 03VariableDeclaration.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    int age; // Complete the variable declaration
+
+    cout << "Enter your age: ";
+    cin >> age;
+    cout << "Thank you!";
+
+    return 0;
+}
+
+
