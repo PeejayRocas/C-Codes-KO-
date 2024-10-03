3b618d37a2365d8371c26bdcdcedce80520c3af4
PeejayRocas <rocaspeejay8@gmail.com>
2024-10-03 09:43:09 +0800

    02BasicOutput.cpp

---
+++ 02BasicOutput.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Our Lady of Lourdes College Foundation" <<endl;
+	cout << "College of Computer Science" <<endl;
+	cout << "BSIT - 1st Year College" <<endl;
+	cout << "Peejay B. Rocas" <<endl;
+	cout << "P-1 Brgy Malasugui Labo Camarenes Norte" <<endl;
+	
+	return 0;
+
+}
