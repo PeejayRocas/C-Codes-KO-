d0810f6fa779bfd89a8163b778517972b993f3b8
PeejayRocas <rocaspeejay8@gmail.com>
2024-11-19 09:20:17 +0800

    Update ACTIVITY_2___PEEJAY.cpp

--- ACTIVITY_2___PEEJAY.cpp
+++ ACTIVITY_2___PEEJAY.cpp
@@ -5,20 +5,14 @@ using namespace std;
 int main() 
 {
     int age;
-    
     cout << "What is your age: ";
     cin >> age;
-    
     if(age < 16) {
-        cout << "Too young to drive" << endl;
-    } 
-	else if (age >= 16) {
+        cout << "Too young to drive" << endl; 
+    else if (age == 16) {
         cout << "Better clear the road" << endl;
-    } 
-	else { 
+     else if(age > 16)
         cout << "You are getting pretty old" << endl;
-    }
-    
-    return 0;
+     return 0;
 }
 
