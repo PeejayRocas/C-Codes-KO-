b6bfe884a2ae14e4b5de9202b27f6a0a8d1b47db
PeejayRocas <rocaspeejay8@gmail.com>
2024-11-26 08:50:14 +0800

    Update ACTIVITY_1___PEEJAY.cpp

--- ACTIVITY_1___PEEJAY.cpp
+++ ACTIVITY_1___PEEJAY.cpp
@@ -1,23 +1,20 @@
 #include <iostream>
-
 using namespace std;
 
-int main() 
-{
-    int temperature;
-    
-    cout << "Enter the temperature in degree Fahrenheit: ";
+int main() {
+    double temperature;
+
+    cout << "Enter the temperature in degrees: ";
     cin >> temperature;
-    
-    if(temperature <= 32) {
-        cout << "Bring a heavy jacket" << endl;
-    } 
-	else if (temperature > 32 && temperature <= 50) {
-        cout << "Bring a light jacket" << endl;
-    } 
-	else {
-        cout << "No jacket needed" << endl;
+
+    if (temperature <= 32) {
+        cout << "Bring a heavy jacket." << endl;
+    } else if (temperature < 50) {
+        cout << "Bring a light jacket." << endl;
+    } else {
+        cout << "No jacket needed." << endl;
     }
-    
+
     return 0;
 }
+
